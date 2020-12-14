676dd461c0293649379610c283affb82ba25712d
saurabhshinde99 <saurabhshinde208@gmail.com>
2020-12-14 14:24:50 +0530

    merged html file with js and frontend work complete

--- static/js/insert.js
+++ static/js/insert.js
@@ -1,22 +1,33 @@
 $(document).ready(function () {
     $.post("/polls/", function (response) {
         // alert(response);
-        console.log(response);
+        console.log(response.movie_data);
         // $("#img0").attr('src', 'https://image.tmdb.org/t/p/w500/' + (response.results[4].poster_path)).height(250).width(250)
         // $("#original_title0").append(response.results[4].original_title)
         // $("#popularity0").append(response.results[4].popularity)
         // $("#overview0").append(response.results[4].overview)
 
-        //Appending
-        $.each(response.movie_data, function (index, value) {
-            // console.log(value.original_title);
-            // console.log(value.popularity);
-            // console.log(value.overview);
+        //using loop
+        // for (index = 0; index < response.movie_data["length"]; index++) {
+        //     // console.log(value_new.results[index].title);
+        //     $("#name" + index).append(response.movie_data[index].title)
+        //     $("#date" + index).append(response.movie_data[index].release_date)
+        //     $("#overview" + index).append(response.movie_data[index].overview)
+        //     $("#popularity" + index).append(response.movie_data[index].popularity)
+        //     $("#img" + index).attr('src', 'https://image.tmdb.org/t/p/w500/' + (response.movie_data[index].poster_path))
 
-            // var img=$(this).attr('src', 'https://image.tmdb.org/t/p/w500/' + (response.results[index].poster_path));
-            // console.log(img[0])
-            // console.log(value.poster_path)
-            $(".container").append(`<div class="card" style="width: 18rem;">
+
+
+            //Appending
+            $.each(response.movie_data, function (index, value) {
+                console.log(value);
+                // console.log(value.popularity);
+                // console.log(value.overview);
+
+                // var img=$(this).attr('src', 'https://image.tmdb.org/t/p/w500/' + (response.results[index].poster_path));
+                // console.log(img[0])
+                // console.log(value.poster_path)
+                $(".container").append(`<div class="card" style="width: 18rem;">
             <img class="card-img-top" id="img" src="https://image.tmdb.org/t/p/w500//${value.poster_path}" alt="Card image cap"></img>
             <div class="card-body">
                 <h5 class="card-title" id="original_title">${value.title}</h5>
@@ -24,7 +35,7 @@ $(document).ready(function () {
                 <p class="card-text" id="overview">${value.overview}</p>
             </div>
         </div>`)
+            });
         });
-    });
 
 });
